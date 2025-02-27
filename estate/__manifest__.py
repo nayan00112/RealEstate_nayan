4332ff01e599b17543fb680c24b548932baa64aa
nayan00112 <nayanpatel00112@gmail.com>
2025-02-27 21:56:18 +0530

    security added for user

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,7 +7,9 @@
         "web",
     ],
     'data':[ 
+        'security/user_groups.xml',
         'security/ir.model.access.csv',
+        'security/ir_rules.xml',
         'views/estate_property_views.xml',
         'views/estate_property_view_kanban.xml',
         'views/estate_property_type_view.xml',
